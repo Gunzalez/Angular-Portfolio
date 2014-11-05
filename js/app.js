94a26a8d4c356c55342dbe39002de68d430b1dcd
Segun Konibire <segun.konibire@springstudios.com>
2014-11-05 03:07:00 +0000

    Now uses  and better name spacing'

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 
-var portfolioApp = angular.module('portfolioApp', []);
+angular.module('portfolioApp', [])
 
-portfolioApp.controller('PortfolioListCtrl', function ($scope) {
-
-    $scope.examples = gunzalez.data.getExamples();
-
-    $scope.social = gunzalez.data.social;
-
-});
+    .controller('PortfolioListCtrl', function ($scope, $http) {
+        $http.get('js/data/examples.json').success(function(data) {
+            $scope.examples = data.sort(function() {
+                return 0.5 - Math.random();
+            });
+        });
+        $http.get('js/data/social.json').success(function(data) {
+            $scope.social = data;
+        });
+    });
 
