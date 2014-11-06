8b98985537be55d664f0bc91302242fef0a6b0b7
Segun Konibire <segun.konibire@springstudios.com>
2014-11-06 02:28:41 +0000

    Adds scroll to top

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 
 angular.module('portfolioApp', [])
 
-    .controller('PortfolioListCtrl', function ($scope, $http) {
-        $http.get('js/data/examples.json').success(function(data) {
-            $scope.examples = data.sort(function() {
-                return 0.5 - Math.random();
+    .run(['$anchorScroll', function($anchorScroll) {
+        $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
+    }])
+
+    .controller('PortfolioListCtrl', ['$anchorScroll', '$scope', '$http',
+        function ($anchorScroll, $scope, $http) {
+            $http.get('js/data/examples.json').success(function(data) {
+                $scope.examples = data.sort(function() {
+                    return 0.5 - Math.random();
+                });
             });
-        });
-        $http.get('js/data/social.json').success(function(data) {
-            $scope.social = data;
-        });
-        $scope.detail = {
-            "display": false,
-            "title":"",
-            "image":"",
-            "description":""
-        };
-        $scope.showDetails = function(index, event){
-            event.preventDefault();
-            $scope.detail.title = $scope.examples[index].name;
-            $scope.detail.image = $scope.examples[index].image;
-            $scope.detail.description = $scope.examples[index].description;
-            $scope.detail.display = true;
-        };
-        $scope.hideDetails = function(){
-            $scope.detail.display = false;
+            $http.get('js/data/social.json').success(function(data) {
+                $scope.social = data;
+            });
+            $scope.detail = {
+                "display": false,
+                "title":"",
+                "image":"",
+                "description":""
+            };
+            $scope.showDetails = function(index, event){
+                event.preventDefault();
+                $scope.detail.title = $scope.examples[index].name;
+                $scope.detail.image = $scope.examples[index].image;
+                $scope.detail.description = $scope.examples[index].description;
+                $scope.detail.display = true;
+                $anchorScroll();
+            };
+            $scope.hideDetails = function(){
+                $scope.detail.display = false;
+            }
         }
-    });
+    ]);
 
